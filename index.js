072ca6107641bf1bb0846975ae95f5a35e6c1fb8
Kamalesh <kamaleshbabu622@gmail.com>
2025-12-23 17:14:21 +0530

    Add Gitignore and html-css folders

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import express from "express";
 import axios from "axios";
+import path from "path";
+
+const dirname = path.resolve();
 
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
+});
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve("public/html/home.html"));
+});
+
+app.get("/task",async (req, res)=>{
+
 });
